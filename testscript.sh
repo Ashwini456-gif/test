541695741f64a855572450125d0e29e5a0e99524
Ashwini <ashwinisekar1999@gmail.com>
2021-04-23 10:29:03 +0000

    adding a test script

---
+++ testscript.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "starting thr script"
+echo "###############################"
+useradd testuser
+echo "ABC user has been created"
+touch /tmp/jenkinstestagain
+echo "File has been created"
+chmod 777 /tmp/jenkinstestagain
+echo "Permission has been changed to 777"
+echo "Hello hello hello" >/tmp/jenkinstestagain
+sleep 30
+echo "Build has been completed successfully"
+
